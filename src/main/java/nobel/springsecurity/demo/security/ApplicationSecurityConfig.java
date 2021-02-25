d82bcf77ad0494970adbf0e1a5e9951f0c05457f
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-02-25 20:15:44 +0200

    Add BCrypt password encoder

--- src/main/java/nobel/springsecurity/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/nobel/springsecurity/demo/security/ApplicationSecurityConfig.java
@@ -8,11 +8,18 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -38,7 +45,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected UserDetailsService userDetailsService() {
         UserDetails userDetails = User.builder()
                                     .username("username")
-                                    .password("password")
+                                    .password(passwordEncoder.encode("password"))
                                     .roles("STUDENT")
                                     .build();
 
