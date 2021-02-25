b972ac7c5a17221eeb0ee2873f47e7091e84d819
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-02-25 17:07:12 +0200

    Add InMemoryUserDetails (without encoded password)

--- src/main/java/nobel/springsecurity/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/nobel/springsecurity/demo/security/ApplicationSecurityConfig.java
@@ -1,9 +1,14 @@
 package nobel.springsecurity.demo.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -26,5 +31,17 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.builder()
+                                    .username("username")
+                                    .password("password")
+                                    .roles("STUDENT")
+                                    .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
     }
 }
