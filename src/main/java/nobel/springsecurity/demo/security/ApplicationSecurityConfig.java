03686cca7afe5dfb22b16c107112e5f6a596d4ac
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-02-25 16:49:23 +0200

    Add Basic authentication description

--- src/main/java/nobel/springsecurity/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/nobel/springsecurity/demo/security/ApplicationSecurityConfig.java
@@ -11,6 +11,13 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        /** Basic Authentication :
+         * Basic authentication is a simple authentication scheme built into the HTTP protocol.
+         * The client sends HTTP requests with the Authorization header that contains
+         * the word Basic word followed by a space and a base64-encoded
+            1- You cant logout while using Basic authentication.
+            2- Each request, sends the username and password in the header of it.
+         */
         http
                 .authorizeRequests()
                 .antMatchers("/", "/index", "/css/*", "/js/*")
