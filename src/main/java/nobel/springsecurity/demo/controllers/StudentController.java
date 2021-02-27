feeae5800f58c7cd5e7536a6c88216ca7c119a99
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-02-27 16:10:43 +0200

    Add Get, Post, Put, Delete methods for Student

--- src/main/java/nobel/springsecurity/demo/controllers/StudentController.java
+++ src/main/java/nobel/springsecurity/demo/controllers/StudentController.java
@@ -2,14 +2,9 @@ package nobel.springsecurity.demo.controllers;
 
 
 import nobel.springsecurity.demo.entities.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 @RestController
 @RequestMapping("/students")
@@ -21,10 +16,37 @@ public class StudentController {
                           new Student(3l, "Lotfi"))
     );
 
+    @GetMapping
+    private Set<Student> getAll() {
+        return set;
+    }
+
     @GetMapping("/{id}")
     private Student findById(@PathVariable Long id) {
         return set.stream().filter(student -> student.getId() == id)
                 .findFirst()
                 .orElse(null);
+    }
+
+    @PostMapping
+    private Student saveStudent(@RequestBody Student student) {
+        long nextId = (set.stream().max(Comparator.comparing(s -> s.getId())).get().getId() + 1);
+        student.setId(nextId);
+        set.add(student);
+        return student;
+    }
+
+    @PutMapping("/{id}")
+    private Student updateStudent(@PathVariable Long id, @RequestBody Student student) {
+        Student foundStudent = set.stream().filter(s -> s.getId() == id).findFirst().orElseThrow(NoSuchElementException::new);
+        foundStudent.setUserName(student.getUserName());
+        return foundStudent;
+    }
+
+    @DeleteMapping("/{id}")
+    private void deleteStudent(@PathVariable Long id) {
+        Student studentToDelete = set.stream().filter(s -> s.getId() == id).findFirst().get();
+        if(studentToDelete != null)
+            set.remove(studentToDelete);
     }
 }
